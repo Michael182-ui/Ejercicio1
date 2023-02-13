d9db786cfaadf976ceb67403f12b55a336df9296
michael brandon <brand_inter@yahoo.com>
2023-02-12 22:46:03 -0600

    first commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class Main {
+    public static void main (String [] args)
+    {
+        //tipos de datos
+
+        byte numeroPequenio = 12;
+        System.out.println(numeroPequenio);
+
+        short numeroMediano = 2345;
+        System.out.println(numeroMediano);
+
+        int numero = 98843;
+        System.out.println(numero);
+
+        long numeroGrande = 5442211L;
+        System.out.println(numeroGrande);
+
+        float numeroDecimalFloat = 3253.45F;
+        System.out.println(numeroDecimalFloat);
+
+        double numeroDecimal = 642.67;
+        System.out.println(numeroDecimal);
+
+        boolean flag = true;
+        System.out.println(flag);
+
+        String cadena = "hola a todos";
+        System.out.println(cadena);
+
+    }
+}
